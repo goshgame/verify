cd6e95ada1dc24a53a0160546c2788fb4f03b3c8
tatecai <TateCai@poweronlabs.ai>
2025-10-29 09:49:43 +0800

    feat: change greedypro

---
+++ src/pages/GreedyProPage/const.ts
@@ -0,0 +1,38 @@
+export const GreedyWeights: number[] = [
+  20778, // betType=1,
+  38200, // betType=2,
+  64332, // betType=3,
+  97000, // betType=4,
+  194230, // betType=5,
+  194230, // betType=6,
+  194230, // betType=7,
+  194230, // betType=8,
+  1000, // betType=9,
+  1770, // betType=10,
+];
+
+export const GreedyTotalWeight: number = 1000000;
+
+export const picList = [
+  "",
+  // betType=1
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-00tedrwy2efa-03akpaar6y6n-1761701805432.png",
+  // betType=2
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-0npu8yvzr8ap-00ncq8qjwlvr-1761702332035.png",
+  // betType=3
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-0ly3smttcf6h-00jxjtlvja8r-1761702360595.png",
+  // betType=4
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-06njmlkmul9t-0gu60f1xbncv-1761702397255.png",
+  // betType=5
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-0ym9j1byr7fb-00kcdcc964xj-1761702426151.png",
+  // betType=6
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-07w89qntzlts-04regg709xg6-1761702453516.png",
+  // betType=7
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-0qwjg2mhccug-0zsnz5kix9us-1761702488427.png",
+  // betType=8
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-00ocj1tz8dsg-00i3rkvbfoq3-1761702514572.png",
+  //  betType=9
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-0wb7djt2jsaa-0018af0adcd7-1761618720838.png",
+  //  betType=10
+  "https://static.vizzlive.com/cdn-cgi/image/format=auto,width=65/sys/45-04595g7g77k3-00phdo4x7ewy-1761618650527.png",
+];
